ac9efcd5831136630b4fb95f7ea54b967ae3f65c
lucass <lucass@gmail.com>
2021-04-22 00:48:04 +0200

    Patial commit, random number not updating

--- src/demo.tsx
+++ src/demo.tsx
@@ -1,17 +1,64 @@
 import { Hub } from 'react-plugs'
 import { Subject } from 'rxjs'
+import { scan } from 'rxjs/operators'
 import * as React from 'react'
 
-const propsOutput: Subject<{a: string}> = new Subject()
+const randomNumberSource: Subject<any> = new Subject()
+
+const randomNumberGenerator = {
+    name: "RandomNumberGenerator",
+    outputs: [
+        {
+            name: "number",
+            outputObservable: randomNumberSource
+        }
+    ]
+}
+
+const randomNumberDisplayProps: Subject<any> = new Subject()
+const randomNumberDisplay = {
+    name: "RandomNumberDisplay",
+    inputs: [
+        {
+            source: "RandomNumberGenerator:number",
+            inputSubscriber: (randomNumberGeneratorOutput) => {
+                randomNumberDisplayProps.next({randomNumber: randomNumberGeneratorOutput.randomNumber})
+            }
+        }
+    ],
+    renderer: {
+        props: randomNumberDisplayProps,
+        functionComponent: ({randomNumber}) => <p>Random number: {randomNumber}</p>
+    }
+}
+
+const randomNumberAccumulatorDisplayProps: Subject<any> = new Subject()
+const randomNumberAccumulatorDisplay = {
+    name: "RandomNumberAccumulatorDisplay",
+    inputs: [
+        {
+            source: "RandomNumberGenerator:number",
+            inputSubscriber: (randomNumberGeneratorOutput) => {
+                randomNumberAccumulatorDisplayProps.next(randomNumberGeneratorOutput.randomNumber)
+            }
+        }
+    ],
+    renderer: {
+        props: randomNumberAccumulatorDisplayProps.pipe(scan((acc, newNumber) => ({randomNumbers: acc.randomNumbers.concat(newNumber)}), {randomNumbers: []})),
+        functionComponent: ({randomNumbers}) => <>{randomNumbers.map( (i, index) => <p key={index}>{index}: {i}</p>)}</>
+    }
+}
 
 const hub = new Hub()
 
-hub.plug({
-    name: "Renderable",
-    renderer: {
-        props: propsOutput,
-        functionComponent: ({a}) => <p>{a}</p>
-    }
-})
+hub.plug(randomNumberGenerator)
+hub.plug(randomNumberDisplay)
+hub.plug(randomNumberAccumulatorDisplay)
 
-propsOutput.next({a: "It Works!"})
+randomNumberSource.next({randomNumber: Math.random()})
+
+setInterval( () => {
+    randomNumberSource.next({randomNumber: Math.random()})
+}, 2000)
+
+
